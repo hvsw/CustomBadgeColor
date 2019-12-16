c04062a1bca30a3cfdd27311d76304c4266c9129
Henrique Valcanaia <henriqueindalencio@gmail.com>
2019-12-16 11:20:05 -0300

    Initial Commit

---
+++ CustomBadgeColor/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CustomBadgeColor
+//
+//  Created by Henrique Valcanaia on 2019-12-16.
+//  Copyright © 2019 Henrique Valcanaia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CustomBadgeColor/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CustomBadgeColor
+//
+//  Created by Henrique Valcanaia on 2019-12-16.
+//  Copyright © 2019 Henrique Valcanaia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
